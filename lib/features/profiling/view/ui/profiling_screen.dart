df71a23997b225ac713b0ddd9ef79dd90b6b6ded
Andi Surya <andi502.as@gmail.com>
2023-11-02 21:32:54 +0700

    integrate textfield name and validater

--- lib/features/profiling/view/ui/profiling_screen.dart
+++ lib/features/profiling/view/ui/profiling_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 import 'package:venturo_form_stepper/features/profiling/constants/profiling_assets_constant.dart';
+import 'package:venturo_form_stepper/features/profiling/controllers/profiling_controller.dart';
 import 'package:venturo_form_stepper/shared/styles/color_style.dart';
 
 class ProfilingScreen extends StatelessWidget {
@@ -9,6 +11,8 @@ class ProfilingScreen extends StatelessWidget {
   final assetsConstant = ProfilingAssetsConstant();
   @override
   Widget build(BuildContext context) {
+    var controller = ProfilingController.to;
+
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
@@ -17,27 +21,40 @@ class ProfilingScreen extends StatelessWidget {
             child: Column(
               children: [
                 SizedBox(height: 16.h),
-                TextField(
-                  keyboardType: TextInputType.name,
-                  decoration: InputDecoration(
-                    labelText: 'Nama',
-                    hintText: 'Masukkan nama anda',
-                    floatingLabelBehavior: FloatingLabelBehavior.always,
-                    floatingLabelStyle: const TextStyle(
-                      color: ColorStyle.primary,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.r),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.r),
-                      borderSide: const BorderSide(
-                        color: ColorStyle.primary,
-                        width: 2,
+                Obx(() {
+                  String? errorMessage;
+
+                  errorMessage = controller.nameErrorMessage.value.trim();
+                  if (errorMessage.isEmpty) {
+                    errorMessage = null;
+                  }
+
+                  return TextField(
+                    controller: controller.nameController,
+                    keyboardType: TextInputType.name,
+                    decoration: InputDecoration(
+                      errorText: errorMessage,
+                      labelText: 'Nama',
+                      hintText: 'Masukkan nama anda',
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
+                      floatingLabelStyle: TextStyle(
+                        color: errorMessage != null
+                            ? ColorStyle.danger
+                            : ColorStyle.primary,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.r),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.r),
+                        borderSide: const BorderSide(
+                          color: ColorStyle.primary,
+                          width: 2,
+                        ),
                       ),
                     ),
-                  ),
-                ),
+                  );
+                }),
                 SizedBox(height: 16.h),
                 const TextField(),
                 SizedBox(height: 16.h),
